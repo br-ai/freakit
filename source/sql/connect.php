1234418567a874c3ddfb4f165aeac3f3ed8210a2
br-ai <brandonsitedjeya@.com>
2024-02-07 20:04:30 +0100

    add port

--- source/sql/connect.php
+++ source/sql/connect.php
@@ -3,10 +3,11 @@ function connect() { mysql://mysql:d556b1840b66b3a821be@vps.orbitafrica.org:8723
 $server = 'vps.orbitafrica.org';
 $bdd    = 'freakit';
 $user   = 'mysql';
-$pwd    = 'd556b1840b66b3a821be'; //par defaut WAMP - MAMP ::: root
+$pwd    = 'd556b1840b66b3a821be';
+$port    = 8723; //par defaut WAMP - MAMP ::: root
 
 
-$connect = mysqli_connect($server, $user, $pwd, $bdd);
+$connect = mysqli_connect($server, $user, $pwd, $bdd, $port);
             //mysqli_connect(SERVER, LOGIN, PASS, MABDD);
 if(!$connect) {
     die('Erreur de connection : ' . mysqli_connect_error());
